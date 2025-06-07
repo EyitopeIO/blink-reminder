c44d4e36d01bd62168f2baac47e4fa946ca8697e
EyitopeIO <adeyitope.io@gmail.com>
2025-06-07 21:12:26 +0100

    chore: initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QPixmap
+
+class BlinkReminder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setGeometry(100, 100, 300, 200)  # Fixed window size
+
+        # Load and resize the image to fit the window
+        pixmap = QPixmap("blink_reminder.png").scaled(300, 200, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        self.label = QLabel(self)
+        self.label.setPixmap(pixmap)
+        self.label.resize(pixmap.size())
+
+        self.show_timer = QTimer(self)
+        self.show_timer.timeout.connect(self.show_window)
+        self.show_timer.start(20 * 60 * 1000)  # Every 20 minutes
+
+        self.hide_timer = QTimer(self)
+        self.hide_timer.timeout.connect(self.close)
+
+    def show_window(self):
+        self.show()
+        self.hide_timer.start(3000)  # Hide after 3 seconds
+
+app = QApplication(sys.argv)
+window = BlinkReminder()
+window.show_window()  # Show immediately on start
+sys.exit(app.exec_())
